030857d10c68d42c5ce6905263d8f75516df2697
Fahmin <fehminsamilli0@gmail.com>
2021-10-19 15:18:26 +0400

    Create: (AuthController first version)

--- pilot-backend/routes/api.php
+++ pilot-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::post('/register', [AuthController::class,'register'])->name('register');
+    Route::post('/login', [AuthController::class,'login'])->name('login');
+
+
 });
